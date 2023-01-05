3bee30d7c7cb49612de36a94687249ca6c28dcd7
gjskc2905 <kaustubhs.chaturbhuj@gmail.com>
2023-01-05 15:51:31 -0500

    first commit

---
+++ src/main/java/com/example/DockerDemo/DockerDemoApplication.java
@@ -0,0 +1,21 @@
+package com.example.DockerDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class DockerDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerDemoApplication.class, args);
+	}
+
+	@GetMapping("/messages")
+	public String getMessage(){
+		return "This is docker demo with spring boot";
+	}
+
+}
